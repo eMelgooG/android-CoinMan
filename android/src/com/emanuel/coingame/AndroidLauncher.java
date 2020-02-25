2b32689f18f8c2164e932b79dcc29fa010c3aabf
eMelgooG <lucaciemanueladrian@gmail.com>
2020-02-25 10:13:03 +0200

    Init commit

---
+++ android/src/com/emanuel/coingame/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.emanuel.coingame;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.emanuel.coingame.CoinGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new CoinGame(), config);
+	}
+}
